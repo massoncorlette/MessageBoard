e4f348f9fc048ac55475067a6b6f2bbd5fa846a0
massoncorlette <massoncorlette07@gmail.com>
2025-03-14 03:35:02 -0400

    POST HTTP implemented, the index route handling the rendering of initial messages and POST request from submission of forum, parsing the data to push into messages array object, then redirect to index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
+const express = require('express'); 
+
 const { Router } = require("express");
 
 const messageRouter = Router();
+messageRouter.use(express.urlencoded({ extended: true })); //parsing forum data to req.body
 
 const messages = [
   {
@@ -19,6 +22,12 @@ messageRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+messageRouter.post('/new', (req, res) => {
+
+  messages.push({ text: req.body.messagetext, user: req.body.fname, added: new Date() });
+
+  res.redirect("/");
+})
 
 
 module.exports = messageRouter;
