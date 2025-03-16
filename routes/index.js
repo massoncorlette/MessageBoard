e14b9f5897b165077280eab808823616efb0308b
massoncorlette <massoncorlette07@gmail.com>
2025-03-16 03:14:09 -0400

    adding color randomizer w math.floor for randomized username colors upon POST actions

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,35 @@ messageRouter.use(express.urlencoded({ extended: true })); //parsing forum data 
 const messages = [
   {
     text: "Ahoy Minoy.",
-    user: "Bob",
+    user: "xXBob123Xx",
     addedDate: "3/1/2025",
     addedTime: "1:00 PM",
+    color: "rgb(191, 64, 255)"
   },
   {
     text: "Peace Out!",
-    user: "Tom",
+    user: "TomAnderson",
     addedDate: "3/14/2025",
     addedTime: "6:00 AM",
+    color:" rgb(255, 102, 0)"
   },
+];
+
+const vividColors = [
+  "rgb(255, 255, 0)",  
+  "rgb(204, 255, 0)", 
+  "rgb(0, 255, 255)",  
+  "rgb(255, 20, 147)",
+  "rgb(255, 102, 0)",  
+  "rgb(191, 64, 255)", 
+  "rgb(255, 0, 0)",    
+  "rgb(255, 255, 255)" ,
+  "rgb(173, 255, 47)",  
+  "rgb(255, 215, 0)",   
+  "rgb(250, 128, 114)",
+  "rgb(0, 255, 0)",    
+  "rgb(139, 0, 255)",  
+  "rgb(255, 165, 0)",   
 ];
 
 messageRouter.get("/", (req, res) => {
@@ -33,6 +52,8 @@ messageRouter.post("/new", (req, res) => {
   };
 
   const now = new Date();
+  const randomColor = vividColors[Math.floor(Math.random() * vividColors.length)];
+
 
   const formattedDate = now.toLocaleDateString("en-US");
   const formattedTime = now.toLocaleTimeString("en-US", timeOptions);
@@ -42,6 +63,7 @@ messageRouter.post("/new", (req, res) => {
     user: req.body.fname,
     addedDate: formattedDate,
     addedTime: formattedTime,
+    color: randomColor
   });
 
   res.redirect("/");
